006abe71773f2c19879591c4b82b1c88f8a83241
RanXia-ZJU <ranxia@zju.edu.cn>
2026-04-14 17:12:20 +0800

    Update 3.DESeq2.R

--- RNA_expression/3.DESeq2.R
+++ RNA_expression/3.DESeq2.R
@@ -3,10 +3,7 @@ library(DESeq2)
 data=read.table("merged.count.xls",header=T,sep="\t",row.names=1)
 data=round(data,0)
 
-colnames(data)=gsub('R_22Rv1_batch2','22Rv1',colnames(data))
-colnames(data)=gsub('scTrioSeq2Rna_HEp.2','HepG2',colnames(data))
-
-group=gsub("_[1-9]","",colnames(data))
+group=c(rep('groupA',3),rep('groupB',3))
 sample_info=data.frame(condition=group)
 rownames(sample_info)=colnames(data)
 
@@ -16,5 +13,5 @@ keep=rowSums(counts(dds))>=6
 dds=dds[keep,]
 
 dds=DESeq(dds)
-res=results(dds,contrast=c('condition','HepG2','22Rv1'))
-write.table(res,"DEGseq2.result.HepG2_vs_22Rv1.xls",quote=F,sep="\t")
+res=results(dds,contrast=c('condition','groupA','groupB'))
+write.table(res,"DEGseq2.result.groupA_vs_groupB.xls",quote=F,sep="\t")
