bab44eda167ddbb5010be32f6443a5f0ffb38d89
RanXia-ZJU <ranxia@zju.edu.cn>
2026-04-14 17:46:48 +0800

    Update 4.DEG_plot.R

--- RNA_expression/4.DEG_plot.R
+++ RNA_expression/4.DEG_plot.R
@@ -14,7 +14,7 @@ main_theme <- theme(plot.margin = unit(c(1, 1, 1, 1), 'lines'),
                     strip.text=element_text(face="bold",size=12,family="Times"),
                     legend.text = element_text(family='Times',size = 12),
                     legend.title = element_text(family='Times',size = 12,face = 'bold'))
-data=read.table("DEGseq2.result.HepG2_vs_22Rv1.xls",header=T,sep="\t",row.names=1)
+data=read.table("DEGseq2.result.groupA_vs_groupB.xls",header=T,sep="\t",row.names=1)
 data=na.omit(data)
 for(i in 1:nrow(data)){
 	if(data$padj[i]<0.05){
@@ -29,20 +29,20 @@ for(i in 1:nrow(data)){
 		data$type[i]='N.S.'
 	}
 }
-write.table(data,"DEGseq2.result.HepG2_vs_22Rv1.addType.xls",quote=F,sep="\t")
+write.table(data,"DEGseq2.result.groupA_vs_groupB.addType.xls",quote=F,sep="\t")
 color=c('Upregulated'='FireBrick','Downregulated'='SteelBlue','N.S.'='gray')
 
-gene=read.table("/data/database/hg38/1_gene/geneID_geneName.unique.xls",header=F,sep="\t")
-colnames(gene)=c('geneID','geneName','geneType')
+gene=read.table("geneID_geneName.xls",header=F,sep="\t")
+colnames(gene)=c('geneID','geneName')
 rownames(gene)=gene$geneID
 gene=gene[rownames(data),]
 
 data$geneName=gene$geneName
 
-cosmic=read.table("/data/database/16_COSMIC/CancerGeneCensus.short.xls",header=T,sep="\t")
+interested_genes=c('geneA','geneB','geneC')
 
-data$label=ifelse(data$geneName %in% cosmic$GENE_SYMBOL,data$geneName,'')
+data$label=ifelse(data$geneName %in% interested_genes,data$geneName,'')
 
-pdf("scTrio_seq2.HepG2_vs_22Rv1.DEGs.pdf",height=4,width=6)
+pdf("scTrio_seq2.groupA_vs_groupB.DEGs.pdf",height=4,width=6)
 ggplot(data,aes(x=log2FoldChange,y=-log10(padj)))+geom_point(aes(color=type),size=0.5)+labs(x="log2FoldChange",y="-log10(adjP)",color="Type")+scale_color_manual(values=color)+ggtitle("HepG2 vs 22Rv1")+guides(color=F,size=F)+geom_label_repel(aes(label=label,color=type),max.overlaps=100)+main_theme
 dev.off()
